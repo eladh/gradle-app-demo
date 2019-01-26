d7beec529213b36faed69a93f5da35d5a37bc030
elad hirsch <hirsch.elad@gmail.com>
2019-01-26 23:46:31 +0200

    basic

--- src/main/java/com/jfrog/demogradle/DemoGradleApplication.java
+++ src/main/java/com/jfrog/demogradle/DemoGradleApplication.java
@@ -2,8 +2,10 @@ package com.jfrog.demogradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {EmbeddedMongoAutoConfiguration.class})
+
 public class DemoGradleApplication {
 
 	public static void main(String[] args) {
