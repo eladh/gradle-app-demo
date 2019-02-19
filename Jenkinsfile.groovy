a6bd541550279556672e26c94d239cb5eafe1bff
elad hirsch <hirsch.elad@gmail.com>
2019-02-19 08:53:39 -0800

    basic

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,9 @@
+podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
+        containerTemplate(name: 'gradle', image: 'frekele/gradle:4.7-jdk8u141', command: 'cat', ttyEnabled: true , privileged: true)]) {
 
-    node('jenkins-pipeline') {
+    node('jenkins-gradle-pipeline') {
         // Get Artifactory server instance, defined in the Artifactory Plugin administration page.
-        def server = Artifactory.server "artifactory"
+        def server = Artifactory.server "artifactory-entplus-us-west"
         // Create an Artifactory Gradle instance.
         def rtGradle = Artifactory.newGradleBuild()
         def buildInfo = Artifactory.newBuildInfo()
@@ -12,7 +14,6 @@
 
         stage ('Artifactory configuration') {
             // Tool name from Jenkins configuration
-            rtGradle.tool = "Gradle-4.7"
             // Set Artifactory repositories for dependencies resolution and artifacts deployment.
             rtGradle.deployer repo:'gradle-local', server: server
             rtGradle.resolver repo:'gradle-virtual', server: server
@@ -21,11 +22,14 @@
         }
 
         stage ('Gradle build') {
-            rtGradle.run rootDir: ".", buildFile: 'build.gradle', tasks: 'clean build', buildInfo: buildInfo
-            rtGradle.run rootDir: ".", buildFile: 'build.gradle', tasks: 'artifactoryPublish', buildInfo: buildInfo
+            container('gradle') {
+                rtGradle.run rootDir: ".", buildFile: 'build.gradle', tasks: 'clean build', buildInfo: buildInfo
+                rtGradle.run rootDir: ".", buildFile: 'build.gradle', tasks: 'artifactoryPublish', buildInfo: buildInfo
+            }
         }
 
         stage ('Publish build info') {
             server.publishBuildInfo buildInfo
         }
     }
+}
