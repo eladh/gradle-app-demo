14d2f608361e16f1d6c834f7622e0279617cd515
elad hirsch <hirsch.elad@gmail.com>
2019-02-18 23:33:41 -0800

    basic

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,7 @@
-podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
-        containerTemplate(name: 'gradle', image: 'frekele/gradle:4.7-jdk8u141', command: 'cat', ttyEnabled: true , privileged: true)]) {
 
     node('jenkins-pipeline') {
         // Get Artifactory server instance, defined in the Artifactory Plugin administration page.
-        def server = Artifactory.server "artifactory"
+        def server = Artifactory.server "artifactory-entplus-us-west"
         // Create an Artifactory Gradle instance.
         def rtGradle = Artifactory.newGradleBuild()
         def buildInfo = Artifactory.newBuildInfo()
@@ -14,30 +12,20 @@ podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
 
         stage ('Artifactory configuration') {
             // Tool name from Jenkins configuration
-            rtGradle.tool = "Gradle-4.6"
+            rtGradle.tool = "Gradle-4.7"
             // Set Artifactory repositories for dependencies resolution and artifacts deployment.
             rtGradle.deployer repo:'gradle-local', server: server
             rtGradle.resolver repo:'gradle-virtual', server: server
             rtGradle.usesPlugin = true // Artifactory plugin already defined in build script
             rtGradle.useWrapper = true
-
-
         }
 
         stage ('Gradle build') {
-
-            container('gradle') {
-                sh "gradle clean build"
-                sh 'gradle artifactoryPublish'
-            }
-
-//        rtGradle.run rootDir: ".", buildFile: 'build.gradle', tasks: 'clean build', buildInfo: buildInfo
-//        rtGradle.run rootDir: ".", buildFile: 'build.gradle', tasks: 'artifactoryPublish', buildInfo: buildInfo
-
+            rtGradle.run rootDir: ".", buildFile: 'build.gradle', tasks: 'clean build', buildInfo: buildInfo
+            rtGradle.run rootDir: ".", buildFile: 'build.gradle', tasks: 'artifactoryPublish', buildInfo: buildInfo
         }
 
         stage ('Publish build info') {
             server.publishBuildInfo buildInfo
         }
     }
-}
